40f0cf019afaeb86228e1b83318bdf5001ab0c57
trongthoaivn <52898471+trongthoaivn@users.noreply.github.com>
2021-11-26 07:56:15 +0700

    #update 25

--- Controllers/ManageController.cs
+++ Controllers/ManageController.cs
@@ -96,5 +96,24 @@ namespace Hocgido2T.Controllers
                 return RedirectToAction("Login", "Users");
             }
         }
+        public ActionResult MyLesson()
+        {
+            try
+            {
+                HttpCookie cookie = HttpContext.Request.Cookies.Get("userID");
+                String maTK = cryto.Decrypt(cookie.Value);
+                NguoiDung nd = db.NguoiDungs.FirstOrDefault(p => p.MaTK.Equals(maTK) && p.TaiKhoan.Quyen.Equals("115"));
+                if (nd != null)
+                {
+                    return View(nd);
+                }
+                else
+                    return RedirectToAction("Login", "Users");
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Login", "Users");
+            }
+        }
     }
 }
